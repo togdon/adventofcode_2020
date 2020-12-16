33b3f28f42b7a3e672b9dbcbdbe88ae45f56b8bb
Travis Ogdon <travis.ogdon@iovation.com>
2020-12-16 06:58:20 -0800

    Played around with improving the time on Day 15, made it worse

--- src/bin/day15.rs
+++ src/bin/day15.rs
@@ -1,9 +1,16 @@
+// use std::collections::HashMap;
 use std::fs;
 
+// NOTE:
+// As a hashmap it takes ~27 seconds
+// As a vec it takes ~7 seconds
+
 fn run(data: &Vec<usize>, iterations: usize) -> usize {
+    // let mut last_spoken = HashMap::new();
     let mut last_spoken: Vec<Option<usize>> = vec![None; iterations as usize];
 
     for (index, num) in data.iter().enumerate() {
+        // last_spoken.insert(*num, index);
         last_spoken[*num as usize] = Some(index);
     }
 
@@ -11,13 +18,14 @@ fn run(data: &Vec<usize>, iterations: usize) -> usize {
 
     for iteration in data.len()..((iterations as usize) - 1) {
         let iteration = iteration as usize;
+        // match last_spoken.insert(answer, iteration) {
         match last_spoken[answer] {
             None => {
-                last_spoken[answer as usize] = Some(iteration);
+                last_spoken[answer as usize] = Some(iteration); // comment out for hashmap
                 answer = 0;
             }
             Some(last_spot) => {
-                last_spoken[answer as usize] = Some(iteration);
+                last_spoken[answer as usize] = Some(iteration);  // comment out for hashmap
                 answer = iteration - last_spot;
             }
         }
